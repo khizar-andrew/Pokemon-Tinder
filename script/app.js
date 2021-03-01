89419323cd949765e9303d3f5b900acb7775542b
Andrew Craig <56005072+AndrewCraig101@users.noreply.github.com>
2021-02-28 19:46:14 -0500

    final first draft

--- script/app.js
+++ script/app.js
@@ -1,112 +1,106 @@
-// POKEMON TINDER CAROUSEL PSEUDCODE
-// POKEMON TINDER CAROUSEL PSEUDCODE
-//Absolute bottom goals: Post 20 pokemon as posters to the page.
-// Normal goals
-// step 1
-// Create a app object 
-// Step 2
-// Link API with key to a fetch request and store data in a variable
-// Step 3
-// In html create slide skeleton with divs and img tags
-// Step 4
-// Style title and buttons through css
-// Step 5 
-// Create a for each loop that appends each pokemon in the data variable onto a div slide 
-// Step 6 
-// Create event listeners on buttons that represent like or dislike. You will always see a  new pokemon but not see who you liked or disliked again in the main carousel.
-// Step 7
-// Display pokemkon stats below each card as it is displayed
-// Stretch goals
-// You can like or dislike a card and it either removes the card from the deck or places the like card in a new deck displayed below in another carousel.
-// At the bottom there is a gallery of disliked cards.
-// Add the pokemon bio as if it's a dating bio.
-// const tinderApp = {};
-// tinderApp.baseUrl = 'https://pokeapi.co/api/v2/';
-// tinderApp.data = () => {
-//     const url = new URL(`${tinderApp.baseUrl}/pokemon/lugia`);
-//         // 2.2 Adding URL Params
-//         url.search = new URLSearchParams({
-//             // q: query
-//         })
-//         // 2.3 Fetch API
-//         fetch(url)
-//         .then((res) => {
-//             return res.json();
-//         })
-//         // .then((data) => {
-//             //     // 2.4 Calling displayArt() function, passing in the art data as argument
-//             //     artApp.displayArt(data.artObjects);
-//             // })
-//         }    
-//         console.log(tinderApp.data());
 
-const pokedex = document.getElementById("pokedex");
-console.log(pokedex);
 
-const tinderApp = (displayPokemon) => {
-    for (let i = 1; i <= 50; i++){
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            // console.log(data);
-            const array = []
-            const pokemon = {
-                image: data.sprites['front_default'],
-                name: data.name,
-                id: data.id,
-                height: data.height,
-                type: data.types.map((type) => type.type.name).join(", ")
-            };
-            array.push(pokemon)
-            // console.log(array.length);
-            console.log(pokemon);
-            tinderApp.displayPokemon(array)
-        });
-    }
+// left to do:
+
+// Create a namespace for the app and adjust the code 
+// Display pokemon one at a time on either the like or dislike button 
+// Try and store the liked Pokemon inside an array that is displayed in the second carousel with next and previous  
+// Name the project in whatever naming convention is used for the project.I.e all kabob case or camel etc...
+// delete all pseudo code and uneeded console.logs
+// make responsive with media queries
+// use wrappers on css
+// Possibly use a movie api to create Cage finder below.
+// Make the project live on github, netlify or personal url
+// 
+
+const pokeInterface = document.getElementById("pokeInterface");
+const finalLikedPokemon = []
+let currentPokemon = 0
+
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
 };
-// tinderApp.getUserInput = () => {
-//     const likeButton = document.getElementsByClassName('.carousel__button--next');
-//     likeButton.addEventListener('click', (event) => {
-//         displayPokemon(array);
-//     }) 
-// }
-tinderApp.displayPokemon = (pokemon) => {
-    const ul = document.querySelector('ul')
-    pokemon.forEach((profile) => {
-        const h2 = document.createElement('h2');
-        const li = document.createElement('li');
-        const image = document.createElement('img');
-        const p = document.createElement('p');
-        h2.innerText = profile.name;
-        image.src = profile.image;
-        image.alt = profile.type;
-        p.innerText = profile.height;
-        li.appendChild(h2);
-        li.appendChild(image);
-        li.appendChild(p);
-        ul.appendChild(li);
-    });
-}
 
+const mainTypes = Object.keys(colors);
+
+const tinderApp = () => {
+
+    const promises = [];
+    for (let i = 1; i <= 7; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url)
+        .then((res) => res.json()));
+    }
+    Promise.all(promises).then(results => {
+        const pokemon = results.map(data => ({
+            name: data.name.toUpperCase(),
+            id: data.id,
+            height: data.height,
+            image: data.sprites['front_default'],
+            pokeType: data.types.map((type) => type.type.name).join(" and ")
+           
+        }));
+        console.log(pokemon);
+        displayPokemon(pokemon)
+    });
+};
+
+
+const displayPokemon = (pokemon) => {
+    pokemon.forEach((pokeOne, index) => {
+        let newEl = document.createElement('li');
+        newEl.classList.add('card');
+        if (index === 0) {
+            newEl.classList.add('is-visible');
+        }
+        newEl.innerHTML = `<img class="card-image" src="https://pokeres.bastionbot.org/images/pokemon/${pokeOne.id}.png"/>
+        <h2 class="card-title">${pokeOne.id}. ${pokeOne.name}</h2>
+        <p class="card-subtitle"> Hey! I am a ${pokeOne.pokeType} type Pokemon and my height is ${pokeOne.height} inches tall!</p>`
+        pokeInterface.appendChild(newEl);
+    });
+        }
+
+
+        function showNextPokemon() {
+            currentPokemon++;
+            let pokemons = document.querySelectorAll('.card');
+            pokemons.forEach((item, index) => {
+                console.log(item, index);
+                if (index === currentPokemon) {
+                    item.className = 'card is-visible';
+                } else {
+                    item.className = 'card';
+                }
+            });
+        }
+        const likeButton = document.querySelector('.carousel__button--like');
+        const disLikeButton = document.querySelector('.carousel__button--dislike');
+        const secondCarousel = document.querySelector('#pokeInterface2')
+        
+        likeButton.addEventListener('click', function () {
+            const likedPoke = document.querySelector('.is-visible');
+            showNextPokemon();
+
+            secondCarousel.appendChild(likedPoke);
+
+
+        })
+        disLikeButton.addEventListener('click', function () {
+            showNextPokemon();
+        });
+
+    
 tinderApp();
-// const tinderApp = () => {
-//     const promises = [];
-//     for (let i = 1; i <= 150; i++){
-//         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-//         promises.push(fetch(url)
-//         .then((res) => res.json()));
-//     }
-//     Promise.all(promises).then(results => {
-//         const pokemon = results.map(data => ({
-//             name: data.name,
-//             id: data.id,
-//             image: data.sprites['front_default'],
-//             type: data.types.map((type) => type.type.name).join(", ")
-//         }));
-//         displayPokemon(pokemon)
-//     });
-// };
-// const displayPokemon = (pokemon) => {
-//     console.log(pokemon);
-// }
